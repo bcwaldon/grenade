05c146bab6fe080f7a5ff97fef56a678b31e47b8
Brian Waldon <bcwaldon@gmail.com>
2012-08-28 20:15:00 +0000

    p5

--- grenade.sh
+++ grenade.sh
@@ -8,7 +8,7 @@ set -o xtrace
 # Grenade assumes it is running on the system that will be hosting the upgrade processes
 
 ESSEX_DEST=/opt/stack.essex
-ESSEX_DEVSTACK_REPO=http://github.com/openstack-dev/devstack.git
+ESSEX_DEVSTACK_REPO=http://github.com/bcwaldon/devstack.git
 ESSEX_DEVSTACK_BRANCH=stable/essex
 
 FOLSOM_DEST=/opt/stack.folsom
@@ -30,19 +30,18 @@ mkdir -p $ESSEX_DEST
 git clone $ESSEX_DEVSTACK_REPO $ESSEX_DEST/devstack
 cd $ESSEX_DEST/devstack
 git fetch $ESSEX_DEVSTACK_REPO $ESSEX_DEVSTACK_BRANCH && git checkout FETCH_HEAD
+cp -p $GRENADE_DIR/localrc.essex $ESSEX_DEST/devstack/localrc
 
 mkdir -p $FOLSOM_DEST
 git clone $FOLSOM_DEVSTACK_REPO $FOLSOM_DEST/devstack
 cd $FOLSOM_DEST/devstack
 git fetch $FOLSOM_DEVSTACK_REPO $FOLSOM_DEVSTACK_BRANCH && git checkout FETCH_HEAD
+cp -p $GRENADE_DIR/localrc.folsom $FOLSOM_DEST/devstack/localrc
 
-# Set up localrc
-cp -p $GRENADE_DIR/localrc.essex $ESSEX_DEST/devstack/localrc
 cd $ESSEX_DEST/devstack
 ./stack.sh
 
 screen -X quit
 
-cp -p $GRENADE_DIR/localrc.folsom $FOLSOM_DEST/devstack/localrc
 cd $FOLSOM_DEST/devstack
-./stack.sh
+./rejoin-stack.sh
